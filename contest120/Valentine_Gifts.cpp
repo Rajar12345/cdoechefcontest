4c9ac40620abb9b05054dcc39ccc3b77079ef808
Rajar12345 <124551189+Rajar12345@users.noreply.github.com>
2024-02-07 20:34:45 +0530

    Create Valentine_Gifts.cpp

---
+++ contest120/Valentine_Gifts.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int t;
+
+  cin >> t;
+
+  while (t--) {
+    int n;
+
+    cin >> n;
+
+    if (n >= 127) {
+      cout << "YES" << endl;
+    } else {
+      cout << "NO" << endl;
+    }
+  }
+
+  return 0;
+}
