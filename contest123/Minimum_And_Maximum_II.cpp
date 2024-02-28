092eec702ff6e601a15f744da4ab277526639b12
Rajar12345 <124551189+Rajar12345@users.noreply.github.com>
2024-02-28 21:29:32 +0530

    Create Minimum_And_Maximum_II.cpp

---
+++ contest123/Minimum_And_Maximum_II.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t)
+    {
+        long long n;
+        cin >> n;
+        long long sum = n * (n + 1) / 2;
+        sum -= 1;
+        sum += n;
+        cout << sum << endl;
+        t--;
+    }
+
+    return 0;
+}
