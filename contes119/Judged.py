aabae85d64c1d7f15fd0c23c50750072160fe792
Rajar12345 <124551189+Rajar12345@users.noreply.github.com>
2024-03-04 12:02:39 +0530

    Delete contes119 directory

--- contes119/Judged.py
+++
@@ -1,9 +0,0 @@
-n = int(input())
-
-for _ in range(n):  # Use _ to indicate unused loop variable
-   a, b, z, z1, z2 = map(int, input().split())
-
-   if a + b + z + z1 + z2 >= 4:
-       print('YES')
-   else:
-       print('NO')
